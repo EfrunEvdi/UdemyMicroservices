966c2495471aaab99c44859be7426f991aa59195
efrunevdi <119731768+EfrunEvdi@users.noreply.github.com>
2024-10-03 19:44:25 +0300

    25. Ders Sonu

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -12,17 +12,14 @@ namespace FreeCourse.Services.Catalog.Services
     public class CategoryService : ICategoryService
     {
         private readonly IMongoCollection<Category> _categoryCollection;
-
         private readonly IMapper _mapper;
 
         public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
         {
+
             var client = new MongoClient(databaseSettings.ConnectionString);
-
             var database = client.GetDatabase(databaseSettings.DatabaseName);
-
             _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
-            _mapper = mapper;
         }
 
         public async Task<Response<List<CategoryDto>>> GetAllAsync()
--- Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -2,17 +2,11 @@ using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog
 {
@@ -33,7 +27,7 @@ namespace FreeCourse.Services.Catalog
             services.AddAutoMapper(typeof(Startup)); // AutoMapper config
             services.AddControllers();
 
-            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings")); // Sýnýf ile appsetings'i configure ettik.
 
             services.AddSingleton<IDatabaseSettings>(sp =>
             {
